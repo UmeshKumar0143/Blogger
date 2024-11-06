44a5a07b3ecc79812908e719c1e51f8a323ab813
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-11-07 00:11:25 +0530

    Aded Navigation

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,8 +1,13 @@
+"use client"
+import { useRouter } from "next/navigation"
 interface propdata {
     text: string | null
 }
 
 export  default function Button({text}:propdata){
-
-    return <button className="text-gray-300 hover:text-white  bg-black rounded-full px-7 py-3 font-bold sm:text-xl  ">{text}</button>
+    const router = useRouter(); 
+   const HandleClick = () =>{
+        text=="Register"? router.push('/api/auth/signup') : router.push('/api/auth/signin')
+    }
+    return <button onClick={HandleClick} className="text-gray-300 hover:text-white  bg-black rounded-full px-7 py-3 font-bold sm:text-xl  ">{text}</button>
 }
